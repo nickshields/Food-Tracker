a372ae1053e0c08ce747c667b7265b5b4e2e2bdf
Nick Shields <nick.shields@live.com>
2020-02-05 14:05:36 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Nick Shields on 2020-02-05.
+//  Copyright © 2020 Nick Shields. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
